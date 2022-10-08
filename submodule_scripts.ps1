36f36e96563c2cf0cbc7faea81b4d26a9a76c7df
Evan Stoll <evanjsx@gmail.com>
2022-10-08 15:53:55 -0400

    Add submodule_scripts.ps1 helper script

---
+++ submodule_scripts.ps1
@@ -0,0 +1,16 @@
+New-Item -Type SymbolicLink -Force -Target ../external/advanced-prompt-matrix/scripts/advanced_prompt_matrix.py -Path ./scripts/advanced_prompt_matrix.py
+New-Item -Type SymbolicLink -Force -Target ../external/amotile/src/process/implementations/automatic1111_scripts/advanced_seed_blending.py -Path ./scripts/advanced_seed_blending.py
+New-Item -Type SymbolicLink -Force -Target ../external/amotile/src/process/implementations/automatic1111_scripts/prompt_blending.py -Path ./scripts/prompt_blending.py
+New-Item -Type SymbolicLink -Force -Target ../external/animator/scripts/animator.py -Path ./scripts/animator.py
+New-Item -Type SymbolicLink -Force -Target ../external/jtkelm2/scripts/wildcards.py -Path ./scripts/wildcards.py
+New-Item -Type SymbolicLink -Force -Target ../external/loopback_superimpose/loopback_superimpose.py -Path ./scripts/loopback_superimpose.py
+New-Item -Type SymbolicLink -Force -Target ../external/seed_travel/scripts/seed_travel.py -Path ./scripts/seed_travel.py
+New-Item -Type SymbolicLink -Force -Target ../external/shift-attention/scripts/shift_attention.py -Path ./scripts/shift_attention.py
+New-Item -Type SymbolicLink -Force -Target ../external/stable-diffusion-webui-cv2-external-masking-script/external_masking.py -Path ./scripts/external_masking.py
+New-Item -Type SymbolicLink -Force -Target ../external/stable-diffusion-webui-video/videos.py -Path ./scripts/videos.py
+New-Item -Type SymbolicLink -Force -Target ../external/txt2mask/scripts/txt2mask.py -Path ./scripts/txt2mask.py
+New-Item -Type SymbolicLink -Force -Target ../external/txt2vectorgraphics/txt2vectorgfx.py -Path ./scripts/txt2vectorgfx.py
+New-Item -Type SymbolicLink -Force -Target ../external/vid2vid/scripts/vid2vid.py -Path ./scripts/vid2vid.py
+
+New-Item -Type SymbolicLink -Force -Target ../external/txt2img2img/scripts/txt2img2img.py -Path ./scripts/txt2img2img.py
+New-Item -Type SymbolicLink -Force -Target ./external/txt2img2img/txt2img2img -Path txt2img2img
